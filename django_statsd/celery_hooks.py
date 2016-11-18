35a242a4eda036b0b3cd32dd505eb363fbe11584
Niels Van Och <niels.vanoch@gmail.com>
2016-11-18 11:51:22 +0100

    Extend the number of signals and replace task_sent
    This is no longer supported in Celery 4

--- django_statsd/celery_hooks.py
+++ django_statsd/celery_hooks.py
@@ -39,12 +39,52 @@ def on_task_postrun(sender=None, task_id=None, task=None, **kwds):
         statsd.timing('celery.%s.runtime' % task.name, ms)
 
 
+def on_task_success(sender=None, task_id=None, task=None, **kwds):
+    """
+    Handle Celery ``task_success`` signals.
+    """
+    # Increase statsd counter.
+    statsd.incr('celery.%s.success' % task)
+
+
 def on_task_failure(sender=None, task_id=None, task=None, **kwds):
     """
     Handle Celery ``task_failure`` signals.
     """
     # Increase statsd counter.
     statsd.incr('celery.%s.failure' % task)
+
+
+def on_task_retry(sender=None, task_id=None, task=None, **kwds):
+    """
+    Handle Celery ``task_retry`` signals.
+    """
+    # Increase statsd counter.
+    statsd.incr('celery.%s.retry' % task)
+
+
+def on_task_revoked(sender=None, task_id=None, task=None, **kwds):
+    """
+    Handle Celery ``task_revoked`` signals.
+    """
+    # Increase statsd counter.
+    statsd.incr('celery.%s.revoked' % task)
+
+
+def on_task_unknown(sender=None, task_id=None, task=None, **kwds):
+    """
+    Handle Celery ``task_unknown`` signals.
+    """
+    # Increase statsd counter.
+    statsd.incr('celery.%s.unknown' % task)
+
+
+def on_task_rejected(sender=None, task_id=None, task=None, **kwds):
+    """
+    Handle Celery ``task_rejected`` signals.
+    """
+    # Increase statsd counter.
+    statsd.incr('celery.%s.rejected' % task)
 
 
 def register_celery_events():
@@ -54,7 +94,12 @@ def register_celery_events():
         raise ImportError('Cannot import celery.signals. This dependency is required when STATSD_CELERY_SIGNALS'
                           ' is True.')
     else:
-        signals.task_sent.connect(on_task_sent)
+        signals.after_task_publish.connect(on_task_sent)
         signals.task_prerun.connect(on_task_prerun)
         signals.task_postrun.connect(on_task_postrun)
+        signals.task_success.connect(on_task_success)
         signals.task_failure.connect(on_task_failure)
+        signals.task_retry.connect(on_task_retry)
+        signals.task_revoked.connect(on_task_revoked)
+        signals.task_unknown.connect(on_task_unknown)
+        signals.task_rejected.connect(on_task_rejected)
