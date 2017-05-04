94137f2c27eba965ee172e9940ea2624e4d7c35a
Jeroen Op 't Eynde <jeroen@vikingco.com>
2017-05-04 17:06:02 +0200

    Prep for pushing to Pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup
 
 
+# Became django-statsd-unleashed because django-statsd and django-statsd-mozilla are taken on Pypi. ;)
 setup(
-    # Because django-statsd was taken, I called this django-statsd-mozilla.
-    name='django-statsd-mozilla',
-    version='0.4.1',
+    name='django-statsd-unleashed',
+    version='0.4.2',
+    url='https://github.com/vikingco/django-statsd',
+    license='BSD',
     description='Django interface with statsd',
     long_description=open('README.rst').read(),
     author='Andy McKay',
     author_email='andym@mozilla.com',
-    license='BSD',
+    maintainer='Unleashed NV',
+    maintainer_email='operations@unleashed.be',
     install_requires=['statsd >= 2.1.2, != 3.2 , <= 4.0'],
     packages=['django_statsd',
               'django_statsd/patches',
@@ -17,7 +20,6 @@ setup(
               'django_statsd/loggers',
               'django_statsd/management',
               'django_statsd/management/commands'],
-    url='https://github.com/andymckay/django-statsd',
     entry_points={
         'nose.plugins.0.10': [
             'django_statsd = django_statsd:NoseStatsd'
@@ -27,8 +29,9 @@ setup(
     zip_safe=False,
     classifiers=[
         'Intended Audience :: Developers',
-        'Natural Language :: English',
+        'Programming Language :: Python',
         'Operating System :: OS Independent',
-        'Framework :: Django'
-    ]
+        'Environment :: Web Environment',
+        'Framework :: Django',
+    ],
 )
