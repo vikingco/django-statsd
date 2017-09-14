bddfaa7e81407b9b815efda7c30e2118501f80f8
Jeroen Op 't Eynde <jeroen@unleashed.be>
2017-09-14 15:52:55 +0200

    Fix gaieror when using django_statsd.client.null

--- django_statsd/clients/null.py
+++ django_statsd/clients/null.py
@@ -4,5 +4,8 @@ from statsd.client import StatsClient
 class StatsClient(StatsClient):
     """A null client that does nothing."""
 
+    def __init__(self, host, port, prefix=None, **kwargs):
+        self._prefix = prefix
+
     def _after(self, data):
         pass
