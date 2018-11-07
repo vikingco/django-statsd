ef47e3a1c9a44f49cd8fc6e4199588a4329c3609
Timvde <tim_vdeynde@hotmail.com>
2018-11-07 10:45:01 +0100

    Only test against supported django versions

--- run_tests.sh
+++ run_tests.sh
@@ -9,17 +9,13 @@ virtualenv2 venv
 ./venv/bin/pip install -r requirements/requirements_test.txt
 ./venv/bin/pip install 'Django>=1.11,<1.12'
 DJANGO_SETTINGS_MODULE='test_settings' venv/bin/pytest django_statsd
-./venv/bin/pip install 'Django>=1.10,<1.11'
-DJANGO_SETTINGS_MODULE='test_settings' venv/bin/pytest django_statsd
-./venv/bin/pip install 'Django>=1.9,<1.10'
-DJANGO_SETTINGS_MODULE='test_settings' venv/bin/pytest django_statsd
-./venv/bin/pip install 'Django>=1.8,<1.9'
-DJANGO_SETTINGS_MODULE='test_settings' venv/bin/pytest django_statsd
 rm -rf venv
 
 virtualenv3 venv
 ./venv/bin/pip install -e .
 ./venv/bin/pip install -r requirements/requirements_test.txt
+./venv/bin/pip install 'Django>=2.1,<2.2'
+DJANGO_SETTINGS_MODULE='test_settings' venv/bin/pytest django_statsd
 ./venv/bin/pip install 'Django>=2.0,<2.1'
 DJANGO_SETTINGS_MODULE='test_settings' venv/bin/pytest django_statsd
 ./venv/bin/pip install 'Django>=1.11,<1.12'
